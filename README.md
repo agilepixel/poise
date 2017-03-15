a7e93ef7934c1e5bccf8d414242785a4e27af586
Richard Brown <richard@agilepixel.io>
2017-03-15 11:20:43 +0000

    public release

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dave's Webpack Grid #
+
+A quick experiment to try and turn Dave's grid into a reusable module for Webpack
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var directory = path.resolve(__dirname);
+module.exports = {
+  entry: directory + '/style.scss',
+  output: {
+    filename: 'bundle.js',
+    path: directory + '/dist/'
+  },
+  module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
+        }]
+    }
+};
